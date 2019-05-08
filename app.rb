17d5f150d17c8ee84ba83e18e9cb10a6dd5a7efa
Monica Marziano <mmarziano1@gmail.com>
2019-05-08 22:30:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "applesauce"
+  end
+  
+  get '/' do 
+    erb :'index'
+  end
+  
+  post '/checkout' do 
+    @session = session.update(params)
+    
+  end 
 end
